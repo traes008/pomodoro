57bd44daed3479ef940f15d1e0789c45ca265e04
thomas <raes.thomas@gmail.com>
2025-05-07 11:00:30 +0200

    Removed database

--- src/database.py
+++ src/database.py
@@ -1,18 +1,1 @@
-import os
-
-def check_if_file_exists(file_path):
-    return os.path.exists(file_path)
-
-def create_file(file_path):
-    with open(file_path, 'w') as file:
-        file.write('')
-
-def read_file(file_path):
-    with open(file_path, 'r') as file:
-        for line in file:
-            print(line)
-
-def write_to_file(file_path, data):
-    with open(file_path, 'w') as file:
-        file.write(data)
 
